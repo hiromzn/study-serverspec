d1fc47000d85773c78049af7a674f3849885c555
hiromzn <hiromzn@gmail.com>
2019-03-17 12:52:02 +0900

    mod

---
+++ serverspec-run-on-ansible-playbook/simple/spec/BBB/sample_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+#describe command('hostname') do
+  #its(:stdout) { should contain( "#{property['inventory_hostname']}" ) }
+#end
+
+describe file( property["my_name"] ) do # sample of playbook variable
+  it { should be_file }
+end
+
+describe file( property["check_file_name"] ) do # sample of inventory variable
+  it { should be_file }
+end
+
