cdd2c1609eefb2e56b0048ac2a579df4e90ff708
hiromichi-mizuno <hiromichi.mizuno@hpe.com>
2019-03-21 16:12:32 +0900

    mod

--- serverspec-run-on-ansible-playbook/simple_tower/spec/BBB/sample_spec.rb
+++ serverspec-run-on-ansible-playbook/simple_tower/spec/BBB/sample_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
   #its(:stdout) { should contain( "#{property['inventory_hostname']}" ) }
 #end
 
-#describe file( property["my_name"] ) do # sample of playbook variable
+#describe file( property["pb_var_file_name"] ) do # sample of playbook variable
   #it { should be_file }
 #end
 
-describe file( property["check_file_name"] ) do # sample of inventory variable
+describe file( property["host_var_file_name"] ) do # sample of inventory variable
   it { should be_file }
 end
 
